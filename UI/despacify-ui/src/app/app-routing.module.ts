7fab906f8cea03a4f0205c45758421ef644b280f
EtienneBerube <tiennebrb@gmail.com>
2019-01-26 15:14:54 -0500

    Added routes

--- UI/despacify-ui/src/app/app-routing.module.ts
+++ UI/despacify-ui/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {ResultsComponent} from '/results/results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/', component: DashboardComponent},
+  {path: '/playlist', component: ResultsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
