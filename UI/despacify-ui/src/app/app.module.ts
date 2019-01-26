a84ae292ca2943472b8c3ca4c83a9eb399372deb
EtienneBerube <tiennebrb@gmail.com>
2019-01-26 14:20:13 -0500

    created components and services

--- UI/despacify-ui/src/app/app.module.ts
+++ UI/despacify-ui/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
