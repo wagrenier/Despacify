cabb3b690661fe660725d3d3898c8d4e583c8339
EtienneBerube <tiennebrb@gmail.com>
2019-01-26 21:17:45 -0500

    Fixed routing module to work with dashboard

--- src/main/resources/static/despacify-ui/src/app/app-routing.module.ts
+++ src/main/resources/static/despacify-ui/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {ResultsComponent} from '/results/results.component';
+import {ResultsComponent} from './results/results.component'
 
 const routes: Routes = [
-  {path: '/', component: DashboardComponent},
-  {path: '/playlist/:name', component: ResultsComponent},
+  {path: '', component: DashboardComponent},
+  {path: 'playlist/:name', component: ResultsComponent},
 ];
 
 @NgModule({
