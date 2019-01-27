cd67fc7487de7284f758c6d7582982fb1e510c8a
EtienneBerube <tiennebrb@gmail.com>
2019-01-27 07:05:12 -0500

    otehr shit

---
+++ other bullshit/src/main/java/resources/BaseUrl.java
@@ -0,0 +1,5 @@
+package resources;
+
+public class BaseUrl {
+    public static final String BASE_URL_WEBSITE = "https://despacify.tech/";
+}
--- src/main/java/resources/BaseUrl.java
+++
@@ -1,5 +0,0 @@
-package resources;
-
-public class BaseUrl {
-    public static final String BASE_URL_WEBSITE = "https://despacify.tech/";
-}
