9b3715275d0e6ed79e7d01506f0b5b1ff8131a14
EtienneBerube <tiennebrb@gmail.com>
2019-01-26 22:52:36 -0500

    got all types

--- src/main/resources/static/despacify-ui/src/app/app.module.ts
+++ src/main/resources/static/despacify-ui/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ResultsComponent } from './results/results.component';
+import { ResultStatComponent } from './results/result-stat/result-stat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    ResultsComponent
+    ResultsComponent,
+    ResultStatComponent
   ],
   imports: [
     BrowserModule,
